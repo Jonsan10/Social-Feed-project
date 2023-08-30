cfcaa0cbc83ad6bfcf89b31ae3c14063c01c77a1
Jonsan10 <sanchezjon480@gmail.com>
2023-08-30 08:27:35 -0700

    updated component for navbar.jsx and NavBar.css

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,13 @@
-
+import React from 'react';
+import './NavBar.css';
 
 const NavBar = (props) => {
     return ( 
         <header>
             <div className="nav-header">
                 <h3 style={{margin: '1em'}}>Social
-                <small className="'text-muted">Feed</small></h3>
-
+                <small className="text-muted">Feed</small>
+                </h3>
             </div>
         </header>
      );
